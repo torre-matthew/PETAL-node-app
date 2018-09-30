df0c47e1d28dca59c28b5b7759683761f7d8d8b2
Torre Matthew <torre.matthew@gmail.com>
2018-09-30 10:21:13 -0400

    inquirer working with bands in town API

--- liri.js
+++ liri.js
@@ -1,23 +1,52 @@
 require("dotenv").config();
 let axios = require("axios");
+let inquirer = require("inquirer");
 
 
-function bandInTown() {
-    axios.get("https://rest.bandsintown.com/artists/" + process.argv[3] + "/events?app_id=codingbootcamp").then(
+inquirer.prompt([
+{
+    type: "list",
+    name: "interest",
+    message: "What are you interested in today?",
+    choices: ["Band or Artist Tour Dates", "Song Info", "Movie Details", "Just Do something!"]
+
+}
+
+]).then(function(userInterest){   
+    inputInquirer(userInterest.interest);
+});
+
+
+function bandInTown(bandorartist) {
+    axios.get("https://rest.bandsintown.com/artists/" + bandorartist + "/events?app_id=codingbootcamp").then(
     function(response) {
 
-            for (i = 0; i < 5; i++) {
+            for (i = 0; i < response.data.length; i++) {
                 console.log("");
                 console.log(response.data[i].venue.name);
                 console.log(response.data[i].venue.city + " " + response.data[i].venue.region + " " + response.data[i].venue.country);
                 console.log(response.data[i].datetime);
                 console.log("");
             }
-
     });
 }
 
-if (process.argv[2] === "concert-this") {
-    bandInTown();
+//Take
+function inputInquirer (userInterest) {
+    if (userInterest === "Band or Artist Tour Dates") {
+    
+    inquirer.prompt([
+        {
+            type: "input",
+            name: "inputval",
+            message: "What Artist or Band are you looking for?",
+        }
+            
+        ]).then(function(bandorartist){
+                bandInTown(bandorartist.inputval);
+        
+        }); 
+
+    }
 }
 
