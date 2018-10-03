c66a4caf5bf31fbacb8281c8d3dee774a73b5357
Torre Matthew <torre.matthew@gmail.com>
2018-10-02 21:46:13 -0400

    everything done except moment for formating date and time for band in town

--- liri.js
+++ liri.js
@@ -3,23 +3,29 @@ let axios = require("axios");
 let inquirer = require("inquirer");
 let Spotify = require('node-spotify-api');
 let spotifyKeys = require("./keys.js");
-    console.log(spotifyKeys);
-    console.log(spotifyKeys.spotify.id);
-
-
-
+let fs = require("fs");
 
 inquirer.prompt([
 {
     type: "list",
     name: "interest",
-    message: "What are you interested in today?",
+    message: "Hello! I'm Petal. What are you interested in today?",
     choices: ["Band or Artist Tour Dates", "Song Info", "Movie Details", "Just Do something!"]
 
 }
 
-]).then(function(userInterest){   
-    inputInquirer(userInterest.interest);
+]).then(function(userInterest){  
+    
+    fs.appendFile("log.txt", userInterest.interest + "\n", function(err) {
+        if (err) {
+          return console.log(err);
+        }
+
+        else {
+            inputInquirer(userInterest.interest);        
+        }
+    });
+
 });
 
 
@@ -35,6 +41,7 @@ function bandInTown(bandorartist) {
                 console.log(response.data[i].venue.city + " " + response.data[i].venue.region + " " + response.data[i].venue.country);
                 console.log("Show starts at " + response.data[i].datetime);
                 console.log("");
+                console.log("= = = = = = = = = = = = = =");
             }
         }
 
@@ -78,11 +85,13 @@ let spotify = new Spotify({
         
         if (response.tracks.items.length > 0) {
             for (let i = 0; i < response.tracks.items.length; i++) {    
+                console.log("");
                 console.log("Artist: " + response.tracks.items[i].artists[0].name);    
                 console.log("Song: " + response.tracks.items[i].name);
                 console.log("Album: " + response.tracks.items[i].album.name);    
                 console.log("Preview: " + response.tracks.items[i].preview_url);
-                console.log("===========================================");
+                console.log("");
+                console.log("= = = = = = = = = = = = = =");
             }       
         }
 
@@ -119,6 +128,53 @@ let spotify = new Spotify({
 
 }
 
+function omdbThatFilm (movie) {
+
+    axios.get("https://www.omdbapi.com/?t=" + movie + "&apikey=265b0607")
+    .then(function(response) {
+        
+        if (response.data.Title != undefined) {
+                console.log("");
+                console.log("Title: " + response.data.Title);
+                console.log("Year: " + response.data.Year);
+                console.log(response.data.Ratings[0].Source + ": " + response.data.Ratings[0].Value);
+                console.log(response.data.Ratings[1].Source + ": " + response.data.Ratings[1].Value);
+                console.log("Country: " + response.data.Country);
+                console.log("Language(s): " + response.data.Language);
+                console.log("Plot Summary: " + response.data.Plot);
+                console.log("Cast: " + response.data.Actors);
+                console.log("= = = = = = = = = = = = = =");
+                console.log("");
+        }
+
+        else {
+            console.log("Sorry, I counldn't find any info on " + movie);
+
+            inquirer.prompt([
+                {
+                    type: "confirm",
+                    name: "tryagain",
+                    message: "Try again?",
+                }
+                    
+                ]).then(function(confirmresponse){
+                    
+                    if (confirmresponse.tryagain === true) {
+                        let userInterest = "Movie Details";
+                            inputInquirer(userInterest);
+                } 
+                
+                    else {
+                        console.log("All good! Come back and see me later if you need anything else.");
+                }
+                
+             });
+        }
+
+    });
+}
+
+
 //Take the users response from the initial inquirer prompt to determine what they are prompted with next
 function inputInquirer (userInterest) {
     if (userInterest === "Band or Artist Tour Dates") {
@@ -131,8 +187,16 @@ function inputInquirer (userInterest) {
         }
             
         ]).then(function(bandorartist){
-                bandInTown(bandorartist.inputval);
+            fs.appendFile("log.txt","Band/Artist: " + bandorartist.inputval + "\n" + "==================" + "\n", function(err) {
+
+                if (err) {
+                  return console.log(err);
+                }
         
+                else {
+                    bandInTown(bandorartist.inputval);        
+                }
+            });    
         }); 
 
     }
@@ -146,18 +210,65 @@ function inputInquirer (userInterest) {
             }
                 
             ]).then(function(song){
-                    spotifyThatSong(song.inputval);
-            }); 
-
+                fs.appendFile("log.txt","Song: " + song.inputval + "\n" + "==================" + "\n", function(err) {
+                    
+                    if (err) {
+                      return console.log(err);
+                    }
+            
+                    else {
+                        spotifyThatSong(song.inputval);        
+                    }
+            });
+        });
     }
 
     else if (userInterest === "Movie Details") {
-        console.log("You need to hook up the OMDB portion of this my guy.");
+        inquirer.prompt([
+            {
+                type: "input",
+                name: "inputval",
+                message: "What movie are you interested in?",
+            }
+                
+            ]).then(function(movie){
 
+                fs.appendFile("log.txt","Movie: " + movie.inputval + "\n" + "==================" +"\n", function(err) {
+                    
+                    if (err) {
+                      return console.log(err);
+                    }
+            
+                    else {
+                        omdbThatFilm(movie.inputval);        
+                    }
+                });    
+            });    
     }
 
     else {
-        console.log("You need to hook up the JUST DO IT portion of this my guy.");
+
+        fs.readFile("random.txt", "utf8", function(error, data) {
+
+            // If the code experiences any errors it will log the error to the console.
+                if (error) {
+                return console.log(error);
+                }
+
+                let dataArr = data.split(",");
+          
+                if (dataArr[0] === "Song Info"){
+                    spotifyThatSong(dataArr[1]);
+                }
+
+                else if (dataArr[0] === "Movie Details") {
+                    omdbThatFilm(dataArr[1]);
+                }
+
+                else if (dataArr[0] === "Band or Artist Tour Dates") {
+                    bandInTown(dataArr[1]);
+                }
+          });
     }
 }
 
